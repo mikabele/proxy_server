abfda24ebcd902c1ec181a35366d33273ffd8d51
mikabele <62202135+mikabele@users.noreply.github.com>
2021-03-05 17:53:14 +0300

    add documentation

--- lab4/server.py
+++ lab4/server.py
@@ -5,27 +5,41 @@ import requests
 
 class ProxyServer:
     __api_key = "7e47aaf7a09ef12d041a22ab370175d0"
-    __cache_size = 10
-    __cached_requests = list()
-    # Standard loopback interface address (localhost) If you pass an empty string,
-    # the server will accept connections on all available IPv4 interfaces.
+    __cache_size = 3
+    __cached_requests: list[tuple[str, int]] = list()
     __HOST = ''
-    __PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+    __PORT = 65432
 
-    def create_get_query(self, city: str):
+    def get_query_result(self, city: str) -> int:
+        """
+        Возвращает температуру, полученую с сервера
+
+        :param city: город, где измеряется температура
+        :return: температура
+        """
         params = dict(access_key=self.__api_key, query=city)
         req = requests.get("http://api.weatherstack.com/current", params=params)
         temperature = eval(req.text)["current"]["temperature"]
         self.cache_request(city, temperature)
         return temperature
 
-    def process_client_query(self, client_socket, client_address):
+    def process_client_query(self, client_socket: socket.socket, client_address: str) -> None:
+        """
+        Обрабатывает запрос клиента
+
+        :param client_socket:
+        :param client_address:
+        :return:
+        """
         with client_socket:
             print('Connected by', client_address)
             city = str(client_socket.recv(1024))[2:-1]
-            temperature = self.get_cached_request(city)[1]
+            temperature = self.get_cached_request(city)
             if not temperature:
-                temperature = self.create_get_query(city)
+                print("not from cached requests")
+                temperature = self.get_query_result(city)
+            else:
+                temperature = temperature[1]
             client_socket.sendall(bytes(str(temperature), encoding="UTF-8"))
 
     def cache_request(self, requested_place: str, request_info: int) -> None:
@@ -36,15 +50,15 @@ class ProxyServer:
         :return:
         """
         if len(self.__cached_requests) == self.__cache_size:
-            contains_requested_pace = False
+            contains_requested_place = False
             for place, info in self.__cached_requests:
                 if requested_place == place:
-                    contains_requested_pace = True
-                    self.__cached_requests.remove([place, info])
+                    contains_requested_place = True
+                    self.__cached_requests.remove((place, info))
                     break
-            if not contains_requested_pace:
-                del self.__cached_requests[-1]
-        self.__cached_requests.append(list(requested_place, request_info))
+            if not contains_requested_place:
+                del self.__cached_requests[0]
+        self.__cached_requests.append((requested_place, request_info))
 
     def get_cached_request(self, requested_place: str):
         """ Возвращает запрос из кэшированных запросов для соответствующего города.
@@ -58,7 +72,11 @@ class ProxyServer:
         return None
 
     def connect_to_server(self):
-        print(socket.AF_INET, socket.SOCK_STREAM)
+        """
+        Подключает клиента к прокси-серверу
+
+        :return:
+        """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.__HOST, self.__PORT))
             s.listen(2)
