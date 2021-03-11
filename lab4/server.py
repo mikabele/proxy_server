ae16e744546e7df808ed71e7a9adbd76e5ca0aa0
Ivan Ishchanka <ivan.ishchanka@gmail.com>
2021-03-11 22:25:22 +0300

    comments deleted

--- lab4/server.py
+++ lab4/server.py
@@ -24,12 +24,6 @@ class ProxyServer:
         self.__api_key = os.environ.get("api_key")
 
     def get_query_result(self, city: str) -> int:
-        """
-        Возвращает температуру, полученую с сервера
-
-        :param city: город, где измеряется температура
-        :return: температура
-        """
         params = dict(access_key=self.__api_key, query=city)
         req = requests.get("http://api.weatherstack.com/current", params=params)
         temperature = eval(req.text)["current"]["temperature"]
@@ -37,13 +31,6 @@ class ProxyServer:
         return temperature
 
     def process_client_query(self, client_socket: socket.socket, client_address: str) -> None:
-        """
-        Обрабатывает запрос клиента
-
-        :param client_socket:
-        :param client_address:
-        :return:
-        """
         with client_socket:
             print('Connected by', client_address)
             city = str(client_socket.recv(1024))[2:-1]
@@ -56,12 +43,6 @@ class ProxyServer:
             client_socket.sendall(bytes(str(temperature), encoding="UTF-8"))
 
     def cache_request(self, requested_place: str, request_info: int) -> None:
-        """Кэширует запрос.
-
-        :param requested_place:     название города из зароса.
-        :param request_info:        температура в запрашиваемом городе.
-        :return:
-        """
         if len(self.__cached_requests) == self.__cache_size:
             contains_requested_place = False
             for place, info in self.__cached_requests:
@@ -74,22 +55,12 @@ class ProxyServer:
         self.__cached_requests.append((requested_place, request_info))
 
     def get_cached_request(self, requested_place: str):
-        """ Возвращает запрос из кэшированных запросов для соответствующего города.
-
-        :param requested_place: Название города, по которому делается запрос
-        :return: Если requested_place найдено возвращает последний запрос сделанный для данного города. Иначе - None.
-        """
         for place, info in self.__cached_requests:
             if requested_place == place:
                 return place, info
         return None
 
     def connect_to_server(self):
-        """
-        Подключает клиента к прокси-серверу
-
-        :return:
-        """
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((self.__HOST, self.__PORT))
             s.listen(2)
