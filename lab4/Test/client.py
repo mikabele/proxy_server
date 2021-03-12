5d4b5cbeb61a631e8586cddc78dbe8718fc66a9e
Ivan Ishchanka <ivan.ishchanka@gmail.com>
2021-03-12 23:36:45 +0300

    client edited

--- lab4/Test/client.py
+++ lab4/Test/client.py
@@ -1,17 +1,36 @@
-from concurrent import futures
+import socket
+import configparser
 import requests
+import concurrent.futures
+import urllib.request
 
 
-with futures.ThreadPoolExecutor(max_workers=4) as executor:
-    futures = [
-        executor.submit(
-            lambda: requests.get("http://localhost:65432/temperature?city=Minsk"))
-        for _ in range(8)
-    ]
+class Client:
+    __HOST: str = None
+    __PORT: int = None
+    cities = ["Moscow", "Minsk", "London","Kiev"]
 
-results = [
-    f.result().status_code
-    for f in futures
-]
+    def load_settings(self):
+        configs = configparser.ConfigParser()
+        configs.read("Configs/configs.ini")
+        self.__HOST = configs["client"]["host"]
+        self.__PORT = int(configs["client"]["port"])
 
-print("Results: %s" % results)
+    def integration_test(self):
+        myurls = [f"http://{self.__HOST}:65432/temperature?city={city}" for city in self.cities]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+            futures = [
+                executor.submit(
+                    lambda: requests.get(myurl))
+                for myurl in myurls]
+
+        results = [f.result().status_code for f in futures]
+        print("Results: %s" % results)
+
+    def __init__(self):
+        self.load_settings()
+
+
+if __name__ == "__main__":
+    client = Client()
+    client.integration_test()
